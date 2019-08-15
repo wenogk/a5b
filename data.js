6f8f82e6dd11dd29ce640bbc055767570a9638e1
Romeno <wenogk@nyu.edu>
2019-08-15 20:20:45 +0400

    data urls updated

--- data.js
+++ data.js
@@ -1,13 +1,13 @@
 module.exports = {
-  yusuf:"https://www.google.com",
-  ahmad:"",
-  rumail:"",
-  pingan:"",
-  kevin:"",
-  abdulla:"",
-  ivan:"",
-  yudai:"",
-  zhe:"",
-  kaushal:"",
+  yusuf:"http://www.idesuk.com/how-has-lighting-changed-world/",
+  ahmad:"https://www.ancient-origins.net/ancient-technology/revolutionary-invention-wheel-001713",
+  rumail:"https://www.bbvaopenmind.com/en/articles/internet-changed-everyday-life/",
+  pingan:"https://www.healio.com/endocrinology/news/print/endocrine-today/%7B15afd2a1-2084-4ca6-a4e6-7185f5c4cfb0%7D/penicillin-an-accidental-discovery-changed-the-course-of-medicine",
+  kevin:"https://www.countrylife.co.uk/country-life/compass-flawed-unreliable-changed-the-world-149284",
+  abdulla:"https://www.airspacemag.com/history-of-flight/aircraft-that-changed-the-world-45532020/",
+  ivan:"https://www.vox.com/2017/6/26/15821652/iphone-apple-10-year-anniversary-launch-mobile-stats-smart-phone-steve-jobs",
+  yudai:"http://mentalfloss.com/article/92644/7-unexpected-ways-fire-changed-course-history",
+  zhe:"https://www.printrunner.com/blog/printing-press-changed-world/",
+  kaushal:"https://time.com/4307892/samuel-morse-telegraph-history/",
   romeno:"https://www.wenogk.com"
 }
