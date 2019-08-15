c17f883ed83d2fbd09a16ec2a41a3b214599db7c
Romeno <wenogk@nyu.edu>
2019-08-15 17:02:06 +0400

    redirect

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/redirect/', (req, res) => res.redirect('https://www.google.com'))
+
 app.listen(process.env.PORT || 3000, () => console.log(`Example app listening on port!`))
 
 //http.listen();
