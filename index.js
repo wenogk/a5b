390d1779572ea58da36b28d49523845ccd30bf84
Romeno <wenogk@nyu.edu>
2019-08-15 17:26:14 +0400

    test

--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require('express')
 const app = express()
 
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Welcome to A5B 2ND FLOOR!'))
 
-app.get('/redirect/', (req, res) => res.redirect('https://www.google.com'))
+app.get('/redirect/:person', (req, res) => {
+  let people = {
+    "yusuf":"https://www.google.com",
+    "romeno":"https://www.wenogk.com"
+  }
+  if(req.params.userID in people) {
+    let person =req. params.userID;
+    res.redirect(people.person);
+  } else {
+    res.send('I think you are lost. Welcome to A5B 2ND FLOOR though!')
+  }
+
+});
 
 app.listen(process.env.PORT || 3000, () => console.log(`Example app listening on port!`))
 
